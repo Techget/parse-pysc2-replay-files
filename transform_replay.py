2945e5433aa801691617fbe47e9e706761875a01
Xuan Tong <txmanialife@gmail.com>
2018-04-12 18:48:54 +1000

    fix

--- transform_replay.py
+++ transform_replay.py
@@ -56,7 +56,7 @@ class Parser:
         ping = self.controller.ping()
         self.info = self.controller.replay_info(replay_data)
         if not self._valid_replay(self.info, ping):
-            self.controller.close()
+            self.sc2_proc.close()
             raise Exception("{} is not a valid replay file!".format(self.replay_file_name + '.SC2Replay'))
 
         self.replay_file_name = self.info.map_name+'_'+self.replay_file_name 
