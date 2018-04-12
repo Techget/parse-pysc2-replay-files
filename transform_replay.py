82988da211be95d53904460f0da97a6b46df0962
Xuan Tong <txmanialife@gmail.com>
2018-04-12 18:45:32 +1000

    fix

--- transform_replay.py
+++ transform_replay.py
@@ -60,7 +60,7 @@ class Parser:
             raise Exception("{} is not a valid replay file!".format(self.replay_file_name + '.SC2Replay'))
 
         self.replay_file_name = self.info.map_name+'_'+self.replay_file_name 
-        for player_info in info.player_info:
+        for player_info in self.info.player_info:
             race = sc_common.Race.Name(player_info.player_info.race_actual)
             self.replay_file_name = race + '_' + self.replay_file_name
 
