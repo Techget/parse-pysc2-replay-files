c9486d0e49d7ce369830e2e5ef9e9fd16a3e0086
Xuan Tong <txmanialife@gmail.com>
2018-04-13 20:52:36 +1000

    move parsed replays

--- moveParsedReplayFile.py
+++ moveParsedReplayFile.py
@@ -3,6 +3,7 @@ from os.path import expanduser
 
 home = expanduser("~")
 print(home)
+home += '/'
 parsed_directory = home+'pysc2-replay/data_64/'
 replay_files_directory = home+'StarCraftII/Replays'
 temporary_save_directory = home+'StarCraftII/temp_save_replays'
